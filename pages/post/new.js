967697ed3a1d15757004f72c1e78d25f06187135
Samuel Martins <smbra33@gmail.com>
2024-05-22 22:57:15 -0700

    connect OpenAI api

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,10 +1,26 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../../components/AppLayout";
+import { useState } from "react";
+import Markdown from 'react-markdown';
 
 export default function NewPost(props) {
+    const [postContent, setPostContent] = useState("");
+    const handleClick = async () => {
+        const response = await fetch("/api/generatePost", {
+            method: "POST"
+        });
+        const json = await response.json();
+        setPostContent(json.postContent);
+    }
     return (
         <div>
             <h1>This is the new post page</h1>
+            <button className="btn" onClick={handleClick}>
+                Generate
+            </button>
+            <Markdown>
+                {postContent}
+            </Markdown>
         </div>
     );
 }
